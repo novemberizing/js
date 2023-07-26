97104cd139b0ad46e4b8f868135b9f7a913fea29
novemberizing <novemberizing@gmail.com>
2023-07-26 21:24:28 +0900

    Apply webpack

---
+++ webpack/config.js
@@ -0,0 +1,22 @@
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    mode: "production",
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, '../dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.txt$/,
+                use: 'raw-loader'
+            }
+        ]
+    }
+};
