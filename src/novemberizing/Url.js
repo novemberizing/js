fa3cc420a813e74ce5fe1b0123e6f03dd489dcf6
novemberizing <novemberizing@gmail.com>
2023-08-21 17:53:35 +0900

    window open implement

--- src/novemberizing/Url.js
+++ src/novemberizing/Url.js
@@ -3,4 +3,7 @@ export default class NovemberizingUrl {
     static move(url) {
         location.href = url;
     }
+    static open(url, target = undefined) {
+        window.open(url, target);
+    }
 }
