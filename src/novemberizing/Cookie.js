237ef8763dd5ab860bc10469dfec34bdd79cb512
novemberizing <novemberizing@gmail.com>
2023-08-22 14:03:27 +0900

    Implement novemberizing.cookie.set

--- src/novemberizing/Cookie.js
+++ src/novemberizing/Cookie.js
@@ -1,10 +1,13 @@
 
 export default class NovemberizingCookie {
-    static get(id) {
-        const found = document.cookie.split(";").find(item => item.trim().startsWith(`${id}=`));
+    static get(key) {
+        const found = document.cookie.split(";").find(item => item.trim().startsWith(`${key}=`));
         if(found) {
             return found.split("=")[1];
         }
         return null;
+    }
+    static set(key, value) {
+        document.cookie = `${key}=${value}; max-age=${60*60*24*365}; SameSite=lax;`;
     }
 }
